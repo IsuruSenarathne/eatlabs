77de0f8fd88d05a06f2d0842963c1cdfcc10a68d
Isuru Senarathne <isuru.is77@gmail.com>
2020-02-16 02:04:05 +0530

    added cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,35 @@
 module.exports = function auth(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error("Forbidden");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
-  }
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error("Forbidden");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
 
-  const [username, password] = new Buffer(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  if (username === "admin" && password === "123") {
-    next();
+    const [username, password] = new Buffer.from(
+      authHeader.split(" ")[1],
+      "base64"
+    )
+      .toString()
+      .split(":");
+    if (username === "admin" && password === "123") {
+      res.cookie("user", "admin", { signed: true });
+      next(); // authorized
+    } else {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   } else {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      const err = new Error("Forbidden");
+      err.status = 401;
+      return next(err);
+    }
   }
-}
+};
